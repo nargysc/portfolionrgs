2cc0a0ee9f4b0305f5fe92740c20667221fa8d52
nargysc <32126532+nargysc@users.noreply.github.com>
2024-03-30 11:19:29 +0200

    Contact Component

--- src/Components/FindMe.js
+++ src/Components/FindMe.js
@@ -1,10 +1,54 @@
 import React from "react";
 import transition from "../transition";
+import "./FindMe.scss";
+import Magnetic from "./Magnetic";
 
 function Find() {
   return (
-    <div>
-      <h1> FInd me</h1>
+    <div className="coontact">
+      <div className="bgssgbody" alt="/">
+        <div className="innfo">
+          <span>
+            <span>IF YOU WANT TO SAY HELLO, GET IN TOUCH WITH ME</span>
+            <p>2024 © Edition </p>
+          </span>
+        </div>
+        <div className="sociaals">
+          <div className="sociaalsmenu">
+            <Magnetic>
+              <a
+                href="https://github.com/nargysc"
+                target="_blank"
+                rel="noreferrer"
+              >
+                GitHub
+              </a>
+            </Magnetic>
+          </div>
+          <div className="sociaalsmenu">
+            <Magnetic>
+              <a
+                href="https://www.linkedin.com/in/nargizaydin/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                LinkedIn
+              </a>
+            </Magnetic>
+          </div>
+          <div className="sociaalsmenu">
+            <Magnetic>
+              <a
+                href="https://www.instagram.com/nrgzlife/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                Instagram
+              </a>
+            </Magnetic>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
