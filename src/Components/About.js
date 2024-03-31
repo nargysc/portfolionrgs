3bc141597dac716611903b37305079aa6a3b16b2
nargysc <32126532+nargysc@users.noreply.github.com>
2024-03-31 17:04:38 +0300

    Languages

--- src/Components/About.js
+++ src/Components/About.js
@@ -11,13 +11,7 @@ function About() {
       <h1 className="textp">
         <span>Who am I?</span>
       </h1>
-      <p className="paragraphp">
-        I'm Nargiz - Aspiring Front-End Engineer and sometimes specialty coffee
-        expert. Currently, I'm deeply immersed in Front-End Development, fueled
-        by my passion to create digital experiences that are both immersive and
-        visually captivating, leaving a lasting impact on users even after
-        they've interacted with them.
-      </p>
+      <p className="paragraphp">{t("aboutpp")}</p>
     </div>
   );
 }
